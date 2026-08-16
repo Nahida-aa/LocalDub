b3871b6e573f7e16203a28df8d6d40b7e2524545
aa <Nahida-aa@outlook.com>
2026-08-17 05:08:48 +0800

    修复模态对话框组件bug

--- packages/ui-solid/custom/modal/renderer.tsx
+++ packages/ui-solid/custom/modal/renderer.tsx
@@ -1,41 +1,42 @@
 // import { createStore, useSelector, useStore } from '@tanstack/solid-store';
-import { createStore, } from 'solid-js/store';
-import { Modal, type ModalProps } from './modal';
-import type { JSX } from 'solid-js';
+import { createStore } from "solid-js/store";
+import { Modal, type ModalProps } from "./modal";
+import type { JSX } from "solid-js";
 
 export type ModalState = Omit<
-	ModalProps,
-	'children' | 'Trigger' | 'onOpenChange' | 'defaultOpen'
+  ModalProps,
+  "children" | "Trigger" | "onOpenChange" | "defaultOpen"
 > & {
-	content?: ModalProps['children'];
-	pending?: boolean;
+  content?: ModalProps["children"];
+  pending?: boolean;
 };
 
 export const [modalRendererStore, setStore] = createStore<ModalState>({
-	open: false,
-	pending: false,
+  open: false,
+  pending: false,
 });
 
 const setOpen = (open: boolean) => {
-	setStore('open',open);
+  if (open) {
+    return closeModal();
+  }
+  setStore("open", open);
 };
 
-
-type OpenModalOptions = Omit<ModalState, 'open' | 'content'>;
-export const openModal = (
-	content:  ModalProps['children'] ,
-	options?: OpenModalOptions,
-) => {
-	console.log('openModal');
-	setStore((prev) => ({
-		...options,
-		open: true,
-		content,
-	}));
+type OpenModalOptions = Omit<ModalState, "open" | "content">;
+export const openModal = (content: ModalProps["children"], options: OpenModalOptions = {}) => {
+  console.log("openModal");
+  setStore((prev) => ({
+    ...options,
+    open: true,
+    content,
+  }));
 };
 export const closeModal = () => {
-	console.log('closeModal');
-	setOpen(false);
+  console.log("closeModal");
+  setStore((prev) => ({
+    open: false,
+  }));
 };
 
 // export const useModal = () => {
@@ -44,31 +45,31 @@ export const closeModal = () => {
 // };
 
 export const ModalRenderer = () => {
-	// const {
-	// 	open,
-	// 	pending,
-	// 	setOpen,
-	// 	content,
-	// 	title,
-	// 	description,
-	// 	closeOnOverlayClick,
-	// 	size,
-	// 	showCloseButton,
-	// 	...props
-	// } = useModal();
-	return (
-		<Modal
-			{...modalRendererStore}
-			children={modalRendererStore.content}
-			onOpenChange={setOpen}
-			// open={modalRendererStore.open}
-			// title={modalRendererStore.title}
-			// description={modalRendererStore.description}
-			// closeOnOverlayClick={modalRendererStore.closeOnOverlayClick}
-			// size={modalRendererStore.size}
-			// children={modalRendererStore.content}
-			// showCloseButton={modalRendererStore.showCloseButton}
-			// pending={modalRendererStore.pending}
-		/>
-	);
+  // const {
+  // 	open,
+  // 	pending,
+  // 	setOpen,
+  // 	content,
+  // 	title,
+  // 	description,
+  // 	closeOnOverlayClick,
+  // 	size,
+  // 	showCloseButton,
+  // 	...props
+  // } = useModal();
+  return (
+    <Modal
+      {...modalRendererStore}
+      children={modalRendererStore.content}
+      onOpenChange={setOpen}
+      // open={modalRendererStore.open}
+      // title={modalRendererStore.title}
+      // description={modalRendererStore.description}
+      // closeOnOverlayClick={modalRendererStore.closeOnOverlayClick}
+      // size={modalRendererStore.size}
+      // children={modalRendererStore.content}
+      // showCloseButton={modalRendererStore.showCloseButton}
+      // pending={modalRendererStore.pending}
+    />
+  );
 };
